3ac5f22df26add3a4a66da820fdd6e095e5fc26c
dedsec995 <amitluhar49@gmail.com>
2026-03-23 18:55:25 -0400

    Done with my work

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print("Hello Mani")
+
+def main():
+    print("Hello Mani")
+    print("Hello Mani")
+    print("Hello Mani")
+
+def test_me():
+    print("Hello Mani")
+    print("Hello Mani")
+    print("Hello Mani")
+
+if __name__ == "__main__":
+    main()
